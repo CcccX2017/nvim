ca8d7e7921bb72c5195b08179fa37a9f00726141
CcccX <nebulapsns@gmail.com>
2024-07-25 23:10:58 +0800

    feat:添加springboot-nvim插件

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,12 +28,15 @@ require("lazy").setup({
         colorscheme = "tokyonight",
       },
     },
+    -- lsp
+    { import = "lazyvim.plugins.extras.lsp.neoconf" },
     -- editor
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     -- coding
     { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- formatting
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.black" },
---
+++ lua/plugins/java.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      jdtls = {
+        handlers = {
+          ["$/progress"] = function(_, result, ctx)
+            local message = result.value.message or ""
+            if message:lower():find("validate documents") or message:lower():find("publish diagnostics") then
+              return
+            end
+            return vim.lsp.handlers["$/progress"](_, result, ctx)
+          end,
+        },
+      },
+    },
+  },
+  {
+    "elmcgill/springboot-nvim",
+    ft = { "java" },
+    lazy = true,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-jdtls",
+    },
+    config = function()
+      local springboot_nvim = require("springboot-nvim")
+
+      vim.keymap.set("n", "<leader>jr", springboot_nvim.boot_run, { desc = "[J]ava [R]un Spring Boot" })
+      vim.keymap.set("n", "<leader>jc", springboot_nvim.generate_class, { desc = "[J]ava Create [C]lass" })
+      vim.keymap.set("n", "<leader>ji", springboot_nvim.generate_interface, { desc = "[J]ava Create [I]nterface" })
+      vim.keymap.set("n", "<leader>je", springboot_nvim.generate_enum, { desc = "[J]ava Create [E]num" })
+
+      springboot_nvim.setup()
+    end,
+  },
+}
