94cd6f91bbe0827a536b68bb1de0fc5963f6417f
CcccX <nebulapsns@gmail.com>
2024-07-30 21:56:09 +0800

    feat:添加s1n7ax/nvim-window-picker插件

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -38,4 +38,45 @@ return {
       { "k", "<Plug>(accelerated_jk_gk)" },
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    opts = {
+      hint = "floating-big-letter",
+      show_prompt = false,
+      filter_rules = {
+        include_current_win = true,
+        bo = {
+          filetype = { "ts-context", "lualine", "fidget", "neo-tree", "NvimTree", "incline", "notify", "noice" },
+        },
+      },
+      filter_func = function(windows, rules)
+        local function predicate(wid)
+          local cfg = vim.api.nvim_win_get_config(wid)
+          if not cfg.focusable then
+            return false
+          end
+          return true
+        end
+        local filtered = vim.tbl_filter(predicate, windows)
+
+        local dfilter = require("window-picker.filters.default-window-filter"):new()
+        dfilter:set_config(rules)
+        return dfilter:filter_windows(filtered)
+      end,
+    },
+    keys = {
+      {
+        "<c-w>p",
+        function()
+          local window_number = require("window-picker").pick_window()
+          if window_number then
+            vim.api.nvim_set_current_win(window_number)
+          end
+        end,
+      },
+    },
+  },
 }
