7971a575061752f46615d98c4652699ca52a99fa
CcccX <nebulapsns@gmail.com>
2024-08-11 20:50:54 +0800

    fix:继续优化logo.lua

--- lua/utils/logo.lua
+++ lua/utils/logo.lua
@@ -40,19 +40,35 @@ local iterm_logo = {
 local term_programs = {
   konsole = "KONSOLE_DBUS_SESSION",
   kitty = "KITTY_PID",
+  alacritty = "TERM",
+  xterm = "TERM",
 }
 
-function M.get_logo()
+local function detect_terminal()
   local term_program = os.getenv("TERM_PROGRAM")
-  if not term_program then
-    for program, env_var in pairs(term_programs) do
-      if os.getenv(env_var) then
-        term_program = program
+  if term_program then
+    return term_program:lower()
+  end
+
+  for program, env_var in pairs(term_programs) do
+    local env = os.getenv(env_var)
+    if env then
+      if env_var == "TERM" then
+        if string.match(env:lower(), program:lower()) then
+          return program
+        end
+      else
+        return program
       end
     end
   end
+  return "unknown"
+end
 
-  if not term_program or string.find(term_program:lower(), "iterm") then
+function M.get_logo()
+  local term_program = detect_terminal()
+
+  if term_program == "unknown" or string.find(term_program:lower(), "iterm") then
     return iterm_logo
   end
 
