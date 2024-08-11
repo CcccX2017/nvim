6af0a9ce86ae436e132163eae46a3df7075a558f
Fingertip tap <nebulapsns@gmail.com>
2024-08-11 17:12:25 +0800

    fix:优化utils/logo.lua的代码

--- lua/utils/logo.lua
+++ lua/utils/logo.lua
@@ -37,8 +37,21 @@ local iterm_logo = {
   [[                                                                              ]],
 }
 
+local term_programs = {
+  konsole = "KONSOLE_DBUS_SESSION",
+  kitty = "KITTY_PID",
+}
+
 function M.get_logo()
   local term_program = os.getenv("TERM_PROGRAM")
+  if not term_program then
+    for program, env_var in pairs(term_programs) do
+      if os.getenv(env_var) then
+        term_program = program
+      end
+    end
+  end
+
   if not term_program or string.find(term_program:lower(), "iterm") then
     return iterm_logo
   end
