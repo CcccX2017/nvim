895524de1e91ec2db7c61a6cab09fcdbcda7896c
CcccX <422865236@qq.com>
2024-09-02 10:12:02 +0800

    fix:将未使用的参数改为_

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,7 +2,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 
-LazyVim.lsp.on_supports_method("textDocument/codeLens", function(client, buffer)
+LazyVim.lsp.on_supports_method("textDocument/codeLens", function(_, buffer)
   -- 初始化并刷新 CodeLens
   vim.lsp.codelens.refresh()
   vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
