d0abcfff0668facfca99c69be0a0c7b74a71a986
CcccX <nebulapsns@gmail.com>
2024-11-11 20:08:59 +0800

    feat:升级LazyVim到13

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
     -- formatting
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.black" },
