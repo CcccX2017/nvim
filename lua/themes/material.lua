93c69044b84ed2d4c2b519e75efbf9fd8685dddf
CcccX <422865236@qq.com>
2024-11-29 19:41:17 +0800

    优化material主题配置

--- lua/themes/material.lua
+++ lua/themes/material.lua
@@ -16,10 +16,12 @@ M.setup = function()
 
   vim.g.material_style = themes.oceanic
 
+  local transparent = vim.g.transparent_enabled
+
   local colors = require("material.colors")
   material.setup({
     disable = {
-      background = vim.g.transparent_enabled,
+      background = transparent,
     },
     async_loading = false,
     styles = {
@@ -62,10 +64,10 @@ M.setup = function()
         bg = colors.editor.selection,
       },
       TreesitterContext = {
-        bg = vim.g.transparent_enabled and "none" or colors.editor.bg,
+        bg = transparent and "none" or colors.editor.bg,
       },
       TreesitterContextLineNumber = {
-        bg = vim.g.transparent_enabled and "none" or colors.editor.bg,
+        bg = transparent and "none" or colors.editor.bg,
       },
       MiniIconsAzure = {
         link = "MiniIconsAzure",
@@ -73,7 +75,34 @@ M.setup = function()
         fg = colors.syntax.fn,
       },
       Pmenu = {
-        bg = vim.g.transparent_enabled and "none" or colors.editor.selection,
+        bg = transparent and "none" or colors.editor.selection,
+      },
+      MasonHeader = {
+        bg = transparent and "none" or colors.main.darkyellow,
+        fg = transparent and colors.main.darkyellow or colors.main.black,
+        bold = true,
+      },
+      MasonMutedBlock = {
+        bg = transparent and "none" or "#888888",
+        fg = transparent and "#888888" or colors.main.black,
+      },
+      MasonHighlightBlockBold = {
+        bold = true,
+        bg = transparent and "none" or "#56b6c2",
+        fg = transparent and "#56b6c2" or colors.main.black,
+      },
+      LazyH1 = {
+        bold = true,
+        bg = transparent and "none" or colors.main.cyan,
+        fg = transparent and colors.main.cyan or colors.editor.title,
+      },
+      LazyButtonActive = {
+        bg = transparent and "none" or colors.main.cyan,
+        fg = transparent and colors.main.cyan or colors.editor.title,
+        bold = true,
+      },
+      NoiceFormatProgressDone = {
+        bg = colors.editor.selection,
       },
     },
   })
