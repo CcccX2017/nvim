333d91180301d5cc57eb01d343e66933b680c66a
CcccX <422865236@qq.com>
2024-12-03 10:09:50 +0800

    优化rose-pine主题配置

--- lua/themes/rose-pine.lua
+++ lua/themes/rose-pine.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.setup = function()
-  local transparent = vim.g.transparent_enabled
   local status_ok, rose_pine = pcall(require, "rose-pine")
 
   if not status_ok then
     return
   end
 
+  local transparent = vim.g.transparent_enabled
   local c = require("rose-pine.palette")
 
   rose_pine.setup({
@@ -17,6 +17,51 @@ M.setup = function()
     highlight_groups = {
       StatusLine = {
         bg = c.base,
+      },
+      LazyH1 = {
+        bg = transparent and "none" or c.gold,
+        fg = transparent and c.gold or c.base,
+        bold = true,
+      },
+      LazyButton = {
+        bg = transparent and "none" or c.overlay,
+        fg = transparent and c.subtle or c.text,
+      },
+      LazyButtonActive = {
+        bg = transparent and "none" or c.highlight_med,
+        fg = transparent and c.rose or c.text,
+      },
+      LazySpecial = {
+        bg = "none",
+        fg = transparent and c.rose or c.subtle,
+      },
+      MasonHeader = {
+        bg = transparent and "none" or c.gold,
+        fg = transparent and c.gold or c.base,
+        bold = true,
+      },
+      MasonMutedBlock = {
+        bg = transparent and "none" or c.muted,
+        fg = transparent and c.subtle or c.text,
+      },
+      MasonHighlightBlockBold = {
+        bg = transparent and "none" or c.rose,
+        fg = transparent and c.rose or c.base,
+        bold = true,
+      },
+      MasonHighlight = {
+        fg = c.rose,
+      },
+      TreesitterContext = {
+        bg = transparent and "none" or c.base,
+      },
+      TreesitterContextLineNumber = {
+        bg = "none",
+        fg = c.rose,
+      },
+      TreesitterContextSeparator = {
+        bg = transparent and "none" or c.base,
+        fg = c.muted,
       },
     },
   })
